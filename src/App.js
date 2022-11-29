41e5e323a57997c2dcfc8c4689eefae27d4a3226
Yoon Robin <robinyoonofficial@gmail.com>
2022-11-29 14:13:42 +0900

    finish consumer-version

--- src/App.js
+++ src/App.js
@@ -34,10 +34,10 @@ function App() {
   return (
       <AuthContext.Provider 
         value={{
-          isLoggedIn: false,
+          isLoggedIn: isLoggedIn,
         }}
       >
-        <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+        <MainHeader onLogout={logoutHandler} />
         <main>
           {!isLoggedIn && <Login onLogin={loginHandler} />}
           {isLoggedIn && <Home onLogout={logoutHandler} />}
