f72382880fad9efff621332f055e514fcf4d0577
Yoon Robin <robinyoonofficial@gmail.com>
2022-11-30 09:22:49 +0900

    finish input-component on my own

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,6 +4,7 @@ import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
 import Button from "../UI/Button/Button";
 import AuthContext from "../../store/auth-context";
+import Input from "../UI/Input/Input";
 
 const emailReducer = (state, action) => {
   if(action.type === 'USER_INPUT'){
@@ -98,21 +99,20 @@ const Login = (props) => {
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-
-        <div
-          className={`${classes.control} ${
-            passwordState.isValid === false ? classes.invalid : ""
-          }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={passwordState.value}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
+        <Input 
+          type= "email"
+          stateValid = {emailState.isValid}
+          value = {emailState.value}
+          onChange = {emailChangeHandler}
+          onBlur={validateEmailHandler}
+        />
+        <Input
+           type= "password"
+           stateValid = {passwordState.isValid}
+           value = {passwordState.value}
+           onChange={passwordChangeHandler}
+           onBlur={validatePasswordHandler}      
+        />
         <div className={classes.actions}>
           <Button type="submit" className={classes.btn} disabled={!formIsValid}>
             Login
--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,7 +1,22 @@
 import classes from './Input.module.css';
 
-const Input = () => {
-    return ;
+const Input = (props) => {
+    return (
+        <div
+        className={`${classes.control} ${
+          props.stateValid === false ? classes.invalid : ""
+        }`}
+      >
+        <label htmlFor={props.type}>E-Mail</label>
+        <input
+          type= {props.type}
+          id={props.type}
+          value={props.value}
+          onChange={props.onChange}
+          onBlur={props.onBlur}
+        />
+      </div>
+    );
 };
 
 export default Input;
